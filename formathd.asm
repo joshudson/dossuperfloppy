aaf5f6dcfd0720779158a3c2958861117ccff375
Joshua Hudson <joshudson@gmail.com>
2021-09-07 20:26:42 -0700

    1.0 release: Adding /F option and rearranged boot loader code to permit repairing more scenarios

--- formathd.asm
+++ formathd.asm
@@ -5,13 +5,13 @@ BITS 16
 
 ORG 0x100
 
-ioerror		equ	0x0790
+ioerror		equ	0x078A
 ioerrorlen	equ	8
-dapatch		equ	0x0798
+dapatch		equ	0x0792
 dapatchlen	equ	6
-missingboot16	equ	0x07A8
+missingboot16	equ	0x07A4
 missingboot16len	equ	8 + 11
-boot16		equ	0x07B0
+boot16		equ	0x07AC
 boot16len	equ	11
 bootdrive	equ	0x01BE
 
@@ -20,20 +20,20 @@ searchsector	equ	secondsector + 512
 parameter	equ	searchsector + 512
 savedsp		equ	parameter + 32
 biosdisk	equ	savedsp + 2
-sloppypatch	equ	biosdisk + 1
+sloppypatch	equ	biosdisk + 1	; Currently unused but keeps alignment
 dapatchtmp	equ	sloppypatch + 1
 ;dapatchtmp is 6 bytes long
 
 ;Custom (near back) values stored for our convenience
 ;The original BPB is surprisingly bad; a few more bytes data would have saved a lot of bytes of code
-%define bpb_rootdirentries	bp + 0x011
-%define mybpb_firstclust	bp + 0x1A6
-%define mybpb_firstroot		bp + 0x1A4
-%define mybpb_sectsperclust	bp + 0x1A2
-%define mybpb_lenstockbpb	bp + 0x1A0
-%define mybpb_physsectorsize	bp + 0x19E
-%define mybpbtmp_nsectors	bp + 0x198
-%define mybpbtmp_nheads		bp + 0x19A
+%define mybpb_firstclust	bp + 0x1A2
+%define mybpb_firstroot		bp + 0x1A0
+%define mybpb_sectsperclust	bp + 0x19E
+%define mybpb_lenstockbpb	bp + 0x19C
+%define mybpb_physsectormult	bp + 0x19A
+%define mybpb_rootdirentries	bp + 0x198
+%define mybpbtmp_nheads		bp + 0x194
+%define mybpbtmp_nsectors	bp + 0x192
 
 
 _start:
@@ -81,7 +81,6 @@ _start:
 	jne	short	.notprephd32
 	jmp	prephd32
 .notprephd32:
-	jmp	prephd32
 	cmp	[si], byte 'F'
 	je	short	.fixboot
 	cmp	[si], byte 'f'
@@ -112,9 +111,8 @@ _start:
 	jmp	format
 
 prephd32:
-	;TODO!
-	mov	ax, 0x4C01
-	int	0x21
+	mov	dx, msg_nofat32
+	jmp	errormsg
 
 prephd16:
 	; Should we adjust partition type for < 65536 sectors? Nah. use the lastest format.
@@ -186,6 +184,7 @@ format:
 	call	checkbx
 	je	short	already
 	call	checkbxmbr
+	jc	.badmbr
 	mov	si, firstsector + 0xDA
 	mov	di, dapatchtmp
 	movsw
@@ -197,16 +196,26 @@ format:
 	call	checkbx
 	jne	short	nogood
 	; TODO: check for space in reserved sectors
-	; TODO: check for FAT32 and say no if no reserved sector is usable
+	mov	ax, [secondsector + 0x16]
+	cmp	[secondsector + 0x16], word 0
+	je	fat32noreserve
 	call	patchfirstsector_b16
-	mov	si, bootloadsig
-	mov	di, secondsector + 4
-	mov	cx, 5
-	rep	movsw
+	mov	al, [transflags]
+	test	al, 0x80
+	jnz	toexiterror
+	mov	bp, firstsector
+	mov	[secondsector], byte 0xEB	; JMP SHORT
+	mov	al, [mybpb_lenstockbpb]
+	inc	al
+	mov	[secondsector + 1], al
+	mov	al, [persistflags]
+	mov	[secondsector + 2], byte al	; Only place we can save something (darn!)
 	call	saveboot16
 	call	savefirstsector
 	mov	ax, 0x4C00
 	int	0x21
+.badmbr mov	dx, msg_badmbr
+	jmp	errormsg
 
 errorcode:
 	shl	ax, 1
@@ -215,7 +224,13 @@ errorcode:
 errormsg:
 	mov	ah, 9
 	int	0x21
+toexiterror:
 	jmp	exit_error
+fat32noreserve:
+fat32noreserve2:
+	; TODO change error message when we have FAT32
+	mov	dx, msg_nofat32
+	jmp	short	errormsg
 	
 already:
 	mov	dx, msg_already
@@ -226,9 +241,159 @@ nogood:
 	jmp	short	errormsg
 
 fixboot:
-	; TODO
-	mov	ax, 0x4C01
+	mov	bx, secondsector
+	call	loadfirstsector
+	mov	si, bx
+	mov	di, firstsector
+	mov	cx, 256
+	rep	movsw
+	call	checkbx
+	; There's two things that can go wrong.
+	; 1: It can be overwritten by a new bootsector
+	; 2: It can be overwritten by a new MBR loader
+	; If they both go wrong at once there's no recovery from here
+	je	short	.isbootloader
+	or	[transflags], byte 0x60
+	mov	bp, firstsector
+	; TODO we can streight up check the high data and see if the reserved sector
+	; is any good
+	jmp	short	.prepboot
+.isbootloader:
+	;TODO try to find bootloader location in reserved sectors
+	cmp	[secondsector + 0x16], word 0
+	je	fat32noreserve2
+	call	patchfirstsector_b16	; Generate first sector
+	mov	al, [transflags]
+	test	al, byte 0x80
+	jz	short	.prepboot
+	jmp	.prepnoboot
+.prepboot:
+	;bp now points to firstsector
+	;find BOOT.16 and check for bootability
+	mov	si, firstsector + dapatch - 0x0600
+	mov	di, dapatchtmp
+	movsw
+	movsw
+	movsw
+	mov	dl, [biosdisk]
+	mov	[bp + 0x1BE], dl
+	call	fat16lbaprep
+	jc	short	.tio1
+	call	.loadboot16
+	jc	short	.prepnoboot16
+	test	[transflags], byte 0xC0
+	jnz	.nosaveregswriteback
+	push	cx		; Save BIOS parameters for writing it back
+	push	dx
+.nosaveregswriteback:
+	mov	al, [searchsector + 2]
+	mov	[persistflags], al
+	jmp	short	.prepmore
+
+.noboot16norec:
+	mov	dx, str_missing
+	jmp	errormsg		; NOT RECOVERABLE
+.prepnoboot16:
+	test	[transflags], byte 0x20
+	jnz	.noboot16norec
+	mov	dx, msg_bootfile
+	or	[transflags], byte 0x80
+.prepnoboot:
+	mov	di, [mybpb_lenstockbpb]
+	add	di, firstsector + 3
+	call	patchfirstsector_noboot_dxmsg
+.prepmore:
+	test	[transflags], byte 0x20
+	jnz	.prepnewmbr
+	mov	bx, firstsector
+	call	checkbxmbr
+	jnc	short	.alreadyfixed
+	mov	dl, [biosdisk]
+	mov	si, dapatchtmp
+	mov	di, firstsector + dapatch - 0x0600
+	movsw
+	movsw
+	movsw
+	mov	di, firstsector + 0x1B0
+	;TODO check for FAT32 (for setting partition type)
+	call	makepartitiontable
+	mov	al, [transflags]
+	test	al, byte 0xC0
+	jnz	short	.nowriteboot16
+	pop	dx
+	pop	cx
+	mov	ax, 0x0301
+	mov	bx, secondsector
 	int	0x21
+.tio1	jc	short	.ioerror
+.nowriteboot16:
+	mov	dh, 0
+	mov	dl, [biosdisk]	; DL Might be trashed
+	mov	cx, 0x0001
+	mov	ax, 0x0301
+	mov	bx, firstsector
+	int	0x13
+	jc	short	.ioerror
+	mov	dx, msg_repairfinished
+	mov	ah, 9
+	int	0x21
+	mov	ax, 0x4C00
+	int	0x21
+.prepnewmbr:
+	;TODO check flag 0x10 for reserved sector
+	call	regenfirstsector_b16
+	jmp	short	.nowriteboot16
+
+.ioerror:
+	jmp	rawioerror
+.alreadyfixed:
+	jmp	already
+.loadboot16:
+	;This code is related to the search code in the bootsector but there were too many variables to make them the same
+	xor	dx, dx
+	mov	ax, [mybpb_firstroot]
+	call	fat16logicaltophysical
+	mov	cx, [mybpb_rootdirentries]
+	jmp	short	.dentrynextsector
+.dentryloop:
+	cmp	bx, searchsector + 0x0200
+	jne	short	.nextdentry
+.dentrynextsector:
+	mov	bx, searchsector
+	push	cx
+	push	ax
+	push	dx
+	call	fat16loadlba
+	jc	short	.ioerror
+	pop	dx
+	pop	ax
+	pop	cx
+	add	ax, 1
+	adc	dx, 0
+.nextdentry:
+	mov	si, bx
+	mov	di, str_boot16
+	push	cx
+	mov	cx, 11
+	repe	cmpsb
+	pop	cx
+	je	short	.founddentry
+	add	bx, 0x20
+	loop	.dentryloop
+.nop	stc
+	ret
+.founddentry:
+	test	[bx + 0x0B], byte 0xD8
+	jnz	short	.nop		; Not a regular file
+	mov	ax, word [bx + 0x1A]
+	or	ax, ax
+	jz	.nop			; Zero sectors
+	call	fat16clustertophysical
+	jc	short	.ioerror
+	mov	bx, searchsector
+	call	fat16loadlba
+	jc	short	.ioerror
+	ret
 
 partitiontype	db	0x06
 
@@ -314,10 +479,11 @@ callformathd:
 	sti
 	cld
 	jc	short	hdrunerror
-	mov	ah, 0x4D
-	int	0x21
-	or	ax, ax
-	jnz	hdexecerror
+	;FORMAT.COM occasionally returns nonzero for success :(
+	;mov	ah, 0x4D
+	;int	0x21
+	;or	ax, ax
+	;jnz	hdexecerror
 	ret
 
 hdrunerror:
@@ -328,9 +494,9 @@ exit_error:
 	mov	ax, 0x4C01
 	int	21h
 
-; These routines should probably be switched to use INT 0x25 and INT 0x26
-; so we can check for stupid user input errors; but I was having trouble
-; earlier. Maybe try again later.
+; As much as I'd like to convert these to use int 0x25 and int 0x26,
+; logical sectors might be bigger than physical sectors causing
+; this code to absolutely break down.
 loadfirstsector:
 	;BX = output buffer
 	mov	dx, 0
@@ -365,33 +531,32 @@ rawioerror:
 	jmp	errormsg
 
 patchfirstsector_b16:
+	;According to the documentation, the first instruction must be a JMP
+	;It can either be a JMP SHORT or a JMP NEAR
 	mov	bx, secondsector
 	mov	bp, firstsector
-	xor	cx, cx
-	mov	al, 0xEB	; JMP SHORT
-	cmp	al, [bx]
+	cmp	[bx], byte 0xEB	; JMP SHORT
 	jne	.try2
-	mov	cx, 2
-	add	cl, byte [bx + 1]
-	jmp	short	.cfold
-.try2	cmp	al, [bx + 1]
-	jne	.try3
-	mov	cx, 3
-	add	cl, byte [bx + 2]
-.cfold	adc	ch, 0
-	jmp	short .cont
-.try3	mov	cx, 3		; Only remaining case must be 0xE9
-	add	cx, word [bx + 1]
+	mov	cl, [bx + 1]
+	mov	ch, 0
+	inc	cx
+	inc	cx
+	jmp	short	.cont
+.try2	mov	cx, [bx + 1]	; must be JMP NEAR
+	cmp	al, [bx]
+	add	cx, 3
 .cont	mov	bx, cx
 	sub	bx, 3
 	mov	[mybpb_lenstockbpb], bx
 	mov	si, secondsector
 	mov	di, bp
 	rep	movsb
+	cmp	di, firstsector + fat16codend - fat16boot + ioerror - 0x0600
+	jae	short	patchfirstsector_noboot
 	mov	bx, di
 	mov	si, fat16boot
-	mov	cx, fat16codend - fat16boot
-	rep	movsb
+	mov	cx, (fat16codend - fat16boot + 1) / 2
+	rep	movsw
 	mov	si, firstsector + 0xDA
 	mov	di, firstsector + dapatch - 0x600
 	movsw
@@ -410,10 +575,12 @@ patchfirstsector_b16:
 	mov	di, firstsector + missingboot16 - 0x0600
 	mov	cx, missingboot16len
 	rep	movsb
-	; Fallthrough: genmybpb
-genmybpb:
-	; Generate my parameters
-	mov	[mybpb_physsectorsize], word 512	; If somebody's game they can maybe make 1K sector sizes work
+	; Generate my BPB parameters (for less code size in boot sector)
+	xor	dx, dx
+	mov	ax, [bp + 0x0B]
+	mov	cl, 9				; If somebody's game they can maybe make 1K sectors work
+	shr	ax, cl
+	mov	[mybpb_physsectormult], ax
 	; Compute distance to root directory
 	mov	ah, 0
 	mov	al, [bp + 0x10]
@@ -421,6 +588,7 @@ genmybpb:
 	add	ax, [bp + 0x0E]
 	mov	[mybpb_firstroot], ax
 	mov	ax, [bp + 0x11]
+	mov	[mybpb_rootdirentries], ax
 	mov	cx, 32
 	mul	cx
 	div	word [bp + 0x0B]
@@ -434,11 +602,86 @@ genmybpb:
 	mov	[mybpb_sectsperclust], ax
 	ret
 
+patchfirstsector_noboot:
+	mov	dx, msg_bigbpb
+patchfirstsector_noboot_dxmsg:
+	mov	ah, 9
+	int	0x21
+	mov	si, errorbpbjmp
+	mov	cx, errorbpbout - errorbpbjmp
+	rep	movsb
+	;si = errorbpbout already
+	mov	di, firstsector + 0x170
+	mov	cx, (errorbpboutend - errorbpbout + 1) / 2
+	rep	movsw
+	mov	di, firstsector + 0x190
+	mov	si, dx
+	mov	cx, 18		; All the error messagse are 36 bytes
+	rep	movsw
+	or	[transflags], byte 0x80
+	ret
+
+regenfirstsector_b16:
+	;This routine only works becasue the W95 routine that trashed the MBR left our data fields in place.
+	mov	si, firstsector + 0xDA
+	mov	di, dapatchtmp
+	movsw
+	movsw
+	movsw
+	mov	si, searchsector
+	mov	di, firstsector
+	mov	bp, di
+	movsw
+	mov	al, 0x90
+	stosb
+	inc	si
+	mov	cx, [mybpb_lenstockbpb]
+	rep	movsb
+	mov	si, fat16boot
+	mov	cx, (fat16codend - fat16boot + 1) / 2
+	rep	movsw
+	mov	si, firstsector + 0xDA
+	mov	di, firstsector + dapatch - 0x600
+	movsw
+	movsw
+	movsw
+	mov	si, dapatchtmp
+	mov	di, firstsector + 0xDA
+	movsw
+	movsw
+	movsw
+	ret
+
 	; The only part of the code that uses DOS file handles
 saveboot16:
 	mov	al, [formatcmddrive]
-	mov	bx, boot16file
-	mov	[bx], al
+	mov	bx, searchsector
+	mov	di, bx		; This loop is longer than storing the
+	stosb			; string constant but makes hex editing
+	mov	ax, ':\'	; the binary to change the file name
+	stosw			; work reliably
+	mov	si, str_boot16
+	movsw
+	movsw
+	movsw
+	movsw
+.lname	cmp	[di - 1], byte ' '
+	jne	short	.ename
+	dec	di
+	jmp	short	.lname
+.ename	mov	al, '.'
+	stosb
+	movsw
+	movsb
+.lext	cmp	[di - 1], byte ' '
+	jne	short	.eext
+	dec	di
+	jmp	short	.lext
+.eext	cmp	[di - 1], byte '.'
+	jne	.nddi
+	dec	di
+.nddi	mov	[di], byte 0
+	;Filename is now built in BX
 	mov	ah, 0x3C
 	mov	cx, 0x07
 	mov	dx, bx
@@ -454,16 +697,13 @@ saveboot16:
 	int	0x21
 	ret
 .cerror	push	ax
-	mov	ah, 0x3E
+	mov	ah, 0x3E	; Close handle before returning to DOS
 	int	0x21
 	pop	ax
 .error	jmp	errorcode
 
 checkbx:	; Check if sector pointed to by BX is DOS formatted or not
-	mov	al, 0xEB	; JMP SHORT
-	cmp	al, [bx]
-	je	.yes
-	cmp	al, [bx + 1]
+	cmp	[bx], byte 0xEB	; JMP SHORT
 	je	.yes
 	cmp	[bx], byte 0xE9	; JMP NEAR
 .yes	ret
@@ -473,9 +713,10 @@ checkbxmbr:	; Check if sector pointed to by BX has a our MBR on it
 	jne	.nope
 	cmp	[bx + 0x1C0], word 0x0001
 	jne	.nope
+	clc
 	ret
-.nope:	mov	dx, msg_badmbr
-	jmp	errormsg
+.nope:	stc
+	ret
 
 %ifdef DEBUG
 hexax:	;DEBUG ROUTINE
@@ -507,12 +748,36 @@ hexax:	;DEBUG ROUTINE
 	pop	ax
 	ret
 .dope	db	0, 0, 0, 0, ' $'
-%ENDIF
 
-bootloadsig	db	'HDSFBSIG'
-persistflags	dw	0
-transflags	dw	0
-msg_usage	db	'FORMATHD formats the entire hard disk as a big single partition, also', 13, 10
+dumpsi11:
+	push	ax
+	push	bx
+	push	cx
+	push	dx
+	push	si
+	mov	dx, 11
+.l	lodsb
+	mov	bx, 7
+	mov	cx, 1
+	mov	ah, 0x0E
+	int	0x10
+	dec	dx
+	jnz	.l
+	pop	si
+	pop	dx
+	pop	cx
+	pop	bx
+	pop	ax
+	ret
+%endif
+
+		align 2, db 0xCC
+;bootloadsig	db	'HDSFBSIG'	; Saved for reserved sector handling
+persistflags	db	0
+transflags	db	0
+msg_nofat32	db	'FAT32 is not implemented yet', 13, 10, '$'
+msg_usage	db	'FORMATHD.COM, version 1.0 Copyright (C) Joshua Hudson 2021', 13, 10
+		db	'FORMATHD formats the entire hard disk as a big single partition, also', 13, 10
 		db	'known as a superfloppy format. This operation must run in two phases', 13, 10
 		db	"because DOS can't reread its partition tables. You must know the BIOS", 13, 10
 		db	'disk number (typically 0-3) and the drive letter it will end up on.', 13, 10
@@ -523,9 +788,12 @@ msg_usage	db	'FORMATHD formats the entire hard disk as a big single partition, a
 		db	'Phase 2: FORMATHD.COM # C: where C: is the drive that # ended up on.', 13, 10
 		db	'Repair: FORMATHD.COM # /F should the OS setup damage the MBR.', 13, 10, '$'
 msg_endphase1	db	'Phase 1 has finished. Reboot and run FORMATHD.COM again.', 13, 10, '$'
+msg_repairfinished	db	'Repair completed', 13, 10, '$'
 msg_already	db	'Boot sector already installed', 13, 10, '$'
 msg_nogood	db	'Boot sector no good', 13, 10, '$'
 msg_badmbr	db	'MBR is no good', 13, 10, '$'
+msg_bigbpb	db	'BPB is too large; disk will not boot', 13, 10, '$'
+msg_bootfile	db	'BOOT    16  lost; disk will not boot', 13, 10, '$'
 msg_not512	db	'Hard disk sector size is not 512 bytes', 13, 10, '$'
 msg_rawioerror	db	'HD IO Error', 13, 10, '$'
 formatexe	db	'A:\FORMAT.COM', 0
@@ -533,11 +801,35 @@ formatcmd:	db	' '
 formatcmddrive	db	'C: /S'
 formatcmdend	db	0
 boot16file	db	'C:\BOOT.16', 0
+		align 2, db 0
 
 %include "errormsg.asm"
+		align 2, db $
 
 ;; DATA: boot module
 ;; Code below here must be relocatable; this is no issue except for the far jump
+errorbpbjmp:
+	jmp	0:0x7D70
+errorbpbout:
+	xor	bx, bx
+	mov	ds, bx
+	mov	es, bx
+	mov	cx, 36
+	mov	si, 0x7D90
+	mov	ah, 0x0E
+	mov	bl, 0x07
+.loop	push	cx
+	lodsb
+	mov	cx, 1
+	int	0x10
+	pop	cx
+	loop	.loop
+	int	0x18
+.hlt	hlt
+	jmp short .hlt
+errorbpboutend:
+	align 2, db 0xCC
+
 _errormbrstart:
 	xor	ax, ax
 	push	ax
@@ -555,6 +847,8 @@ _errormbrstart:
 	jmp	short	.hlt
 .msg	db	'Run FORMATHD.COM from a DOS boot floppy next'
 _errormbrend:
+	align 2, db 0x0
+
 fat16boot:
 	cld
 	xor	bx, bx
@@ -586,7 +880,7 @@ _fat16start:
 	xor	dx, dx
 	mov	ax, [mybpb_firstroot]
 	call	fat16logicaltophysical
-	mov	cx, [bpb_rootdirentries]
+	mov	cx, [mybpb_rootdirentries]
 	jmp	short	.dentrynextsector
 .dentryloop:
 	cmp	bx, 0x7E00
@@ -610,25 +904,23 @@ _fat16start:
 	mov	cx, 11
 	repe	cmpsb
 	pop	cx
-	jne	short	.nom
-	test	[bx + 0x0B], byte 0xD8
-	jz	short	.founddentry
-.nom	add	bx, 0x20
+	je	short	.founddentry
+	add	bx, 0x20
 	loop	.dentryloop
-	mov	si, missingboot16
+.nop	mov	si, missingboot16
 	mov	cx, missingboot16len
 	jmp	short	fat16error
 
 .founddentry:
+	test	[bx + 0x0B], byte 0xD8
+	jnz	short	.nop		; Not a regular file
 	mov	ax, word [bx + 0x1A]
+	or	ax, ax
+	jz	.nop			; Zero sectors
 	call	fat16clustertophysical
 	mov	bx, 0x7C00
 	call	fat16loadlba
 	jc	short	.ioerror
-	mov	si, 0x0603 ; Patch BPB back into boot sector image
-	mov	di, 0x7C03
-	mov	cx, [mybpb_lenstockbpb]
-	rep	movsb
 	mov	cx, 0x0001
 	mov	dh, 0
 	mov	si, 0x600 + bootdrive ; Points to the first MBR entry
@@ -667,12 +959,7 @@ fat16logicaltophysical:
 	; Output: DX:AX
 	; Destroys everything but BP and DI
 	push	dx
-	push	ax
-	mov	ax, [bp + 0x0B]
-	xor	dx, dx
-	div	word [mybpb_physsectorsize]
-	xchg	ax, cx
-	pop	ax
+	mov	cx, [mybpb_physsectormult]
 	mul	cx
 	mov	bx, dx
 	xchg	ax, si	; BX = carry, SI = low output
@@ -726,10 +1013,13 @@ fat16loadlba:
 	int	0x13
 .ret	ret
 fat16codend:
+	align 2, db 0xCC
 
 str_ioerror	db 'HD Error'
 str_missing	db 'Missing '
 str_boot16	db 'BOOT    16 '
+		db 13, 10, '$'	;We print this error from DOS too
+	align 4, db 0
 
 firstsector:
 
